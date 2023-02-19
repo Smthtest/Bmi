30981e726bd151b27e97399712d25a88563ca54e
Your Name <ordoananna@gmail.com>
2023-02-20 00:05:55 +0400

    [200~src/BmiService.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         BmiService service = new BmiService();
         double weight = 60;
-        double height = 157;
+        double height = 1.57;
         double bmi = service.calculate(weight, height);
         System.out.println("Ваш ИМТ равен:");
         System.out.println(bmi);
